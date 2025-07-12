b2f63124a6f843a3ea03e4f92d693cb4a849fc66
masumhossain-dev <masumhossain.dev@gmail.com>
2025-07-12 22:31:54 +0600

    find sum of n th number and find avarage of these numbers

--- avarage_number_of_n.py
+++ avarage_number_of_n.py
@@ -1,1 +1,14 @@
-num = int(input("Enter your numbers: "))
+import math
+num = int(input("Enter any number: "))
+result = 0
+num_list = []
+
+for i in range(num):
+    result = result + i
+    num_list.append(i)
+
+size = len(num_list)
+avarage = result / size
+
+print(avarage)
+print(result)
